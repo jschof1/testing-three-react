cc4cccd6a9264552aa9f62b0d98d63c65d41794c
Jack Schofield <59843981+jschof1@users.noreply.github.com>
2023-02-22 00:08:33 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import * as THREE from "three";
+import React, { useRef, Suspense } from "react";
+import { Canvas, extend, useFrame, useLoader } from "@react-three/fiber";
+import { shaderMaterial, OrbitControls } from "@react-three/drei";
+import glsl from "babel-plugin-glsl/macro";
+import "./styles.scss";
+
+const WaveShaderMaterial = shaderMaterial(
+  // Uniform
+  {
+    uTime: 0,
+    uColor: new THREE.Color(0.0, 0.0, 0.0)
+  },
+  // Vertex Shader
+  glsl`
+    precision mediump float;
+ 
+    varying vec2 vUv;
+    varying float vWave;
+
+    uniform float uTime;
+
+    #pragma glslify: snoise3 = require(glsl-noise/simplex/3d.glsl);
+
+    void main() {
+      vUv = uv;
+
+      vec3 pos = position;
+      float noiseFreq = 2.0;
+      float noiseAmp = 0.4;
+      vec3 noisePos = vec3(pos.x * noiseFreq + uTime, pos.y, pos.z);
+      pos.z += snoise3(noisePos) * noiseAmp;
+      vWave = pos.z;
+
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);  
+    }
+  `,
+  // Fragment Shader
+  glsl`
+    precision mediump float;
+
+    uniform vec3 uColor;
+    uniform float uTime;
+
+    varying vec2 vUv;
+    varying float vWave;
+
+    void main() {
+      float wave = vWave * 0.2;
+
+      gl_FragColor = vec4(uColor, 1.0); 
+    }
+  `
+);
+
+extend({ WaveShaderMaterial });
+
+const Wave = () => {
+  const ref = useRef();
+  useFrame(({ clock }) => (ref.current.uTime = clock.getElapsedTime()));
+
+  return (
+    <mesh>
+      <planeBufferGeometry args={[0.9, 0.4, 50, 50]} />
+      <waveShaderMaterial uColor={"hotpink"} ref={ref} />
+    </mesh>
+  );
+};
+
+const Scene = () => {
+  return (
+    <Canvas camera={{ fov: 12, position: [0, 0, 5] }}>
+      <Suspense fallback={null}>
+        <Wave />
+      </Suspense>
+      <OrbitControls />
+    </Canvas>
+  );
+};
+
+const App = () => {
+  return (
+    <>
+      <h1>SHAPE IT YOUR WAY</h1>
+      <Scene />
+    </>
+  );
+};
+
+export default App;
